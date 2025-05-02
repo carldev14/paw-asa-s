37a202d56e09cb9aa0926381a0babc3659099ce9
carldev14 <carldev.ac@gmail.com>
2025-05-02 16:55:57 +0000

    fix: build errors

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,4 +1,4 @@
 import { Cabin, Space_Grotesk } from "next/font/google";
 
-export const fontPoppins = Cabin({ weight: ["400", "500", "600", "700"] })
-export const fontHeader = Space_Grotesk({ weight: ["500", "600", "700"] })
+export const fontPoppins = Cabin({ weight: ["400", "500", "600", "700"], subsets: ['latin']})
+export const fontHeader = Space_Grotesk({ weight: ["500", "600", "700"], subsets: ['latin'] })
